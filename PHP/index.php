ed20cd0614ae40d810bdc7e852a0f2437f693f2e
GrzegorzBak <grzbak@st.amu.edu.pl>
2015-10-20 12:53:11 +0200

    simple php setup

---
+++ PHP/index.php
@@ -0,0 +1,18 @@
+<html>
+<?php include "structure/header.php" ?>
+<body>
+	<?php include "structure/nav.php" ?>
+	<main>
+
+	</main>
+	<aside>
+		<form action="" method="Post">
+			<label><input type="text" name="username" placeholder="Username"></label>
+			<label><input type="password" name="password" placeholder="Password"></label>
+			<label><input type="email" name="email" placeholder="Email"></label>
+			<input type="submit" value="Start Blogging!">
+		</form> 
+	</aside>
+	<?php include "structure/footer.php" ?>
+</body>
+</html>
