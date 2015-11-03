2fee1c0e2bf3ecb7357525c9a8e486a1ebdfc318
Paweł <owocbananowca@gmail.com>
2015-11-03 12:50:48 +0100

    Update login.php
    json test

--- PHP/functions/login.php
+++ PHP/functions/login.php
@@ -1,4 +1,46 @@
 <?php
 	header('Location: /index.php');
+	
+	//"http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URl'];
+	//"http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+	//Tu adres do wyslałania
+	$url = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URl'];	//???
+	$ch =  curl_init($url);
+	$jsonData = array(
+		$_POST["username"] => "username";
+		$_POST["password"] => "password";
+	);
+	$jsonDataEncoded = json_encode($jsonData);
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+	$result = curl_exec($ch);
+	curl_close($ch);
+	
+	//Odpowiedź z adresu
+	$url2 = '';
+	$ch2 = curl_init($url2);
+	curl_setopt_array($ch2, array(
+		//CURLOPT_RETURNTRANSFER => 1,	//Powoduje zwrócenie true(1) jeśli uda się połączyć i otrzymamy odpowiedź
+		CURLOPT_URL = $url2		//Jeśli się zakomentuje RETURNTRANSFER to chyba dostaniemy string w odpowiedzi
+	));
+	$answer = curl_exec($ch2);
+	//Jeśli dostajemy string z wiadomością spowrotem
+	$answerDecoded = json_decode($answer);
+	/*
+	Proponowana przez Grega odpowiedź:
+	{
+		id:
+		msg:
+		params:
+		info:
+	}
+	*/
+	curl_close(ch2);
+	
+	/*
+	Warunki co ma się stać w zależności od odpowiedzi.
+	*/
+	
 	die();
 ?>
