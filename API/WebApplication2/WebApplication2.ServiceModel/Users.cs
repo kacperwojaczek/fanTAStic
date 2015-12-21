018645a933db5ab5e596fcf56746b1e76a2c8180
kacperwojaczek <wojaczek.kacper@gmail.com>
2015-12-21 19:25:04 +0100

    update user data

--- API/WebApplication2/WebApplication2.ServiceModel/Users.cs
+++ API/WebApplication2/WebApplication2.ServiceModel/Users.cs
@@ -8,10 +8,15 @@ using System.Data.SqlClient;
 namespace WebApplication2.ServiceModel
 {
     [Route("/users/{Login}", "GET")]
-    [Route("/users/{Id}", "PATCH")]
+    [Route("/users/{Login}", "PATCH")]
     public class UserRequest : IReturn<UserResponse>
     {
         public string Login { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Password { get; set; }
+        public string Avatar { get; set; }
+        public string Bio { get; set; }
     }
 
     public class UserResponse
@@ -20,9 +25,10 @@ namespace WebApplication2.ServiceModel
 
         public string Result { get; set; }
 
-        public User Respond(UserRequest request)
+        public User Get(UserRequest request)
         {
             User user = new User();
+
             string connectionString = null;
             SqlConnection cnn;
             connectionString = "Server=tcp:fantastic.database.windows.net,1433;Database=fantastic;User ID=qacpiweb@fantastic;Password=nhm554WW;Trusted_Connection=False;Encrypt=True;Connection Timeout=30;";
@@ -56,6 +62,52 @@ namespace WebApplication2.ServiceModel
                     command = String.Format("Select * from Users where Login='{0}'", request.Login);
                     polecenie = new SqlCommand(command, cnn);
                     dataReader = polecenie.ExecuteReader();
+                    if (dataReader.HasRows)
+                    {
+                        dataReader.Read();
+                        user.Id = dataReader.GetInt32(0);
+                        user.Firstname = dataReader.GetString(1);
+                        user.Lastname = dataReader.GetString(2);
+                        user.Login = dataReader.GetString(4);
+                        user.Password = dataReader.GetString(5);
+                        user.Avatar = dataReader.GetString(6);
+                        user.Bio = dataReader.GetString(7);
+
+                        dataReader.Close();
+                        cnn.Close();
+                        return user;
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+
+        }
+
+        public User Patch(UserRequest request)
+        {
+            User user = new User();
+
+            string connectionString = null;
+            SqlConnection cnn;
+            connectionString = "Server=tcp:fantastic.database.windows.net,1433;Database=fantastic;User ID=qacpiweb@fantastic;Password=nhm554WW;Trusted_Connection=False;Encrypt=True;Connection Timeout=30;";
+            cnn = new SqlConnection(connectionString);
+            try
+            {
+                string command;
+                command = String.Format("Select * from Users where id='{0}'", request.Login);
+                SqlCommand polecenie = new SqlCommand(command, cnn);
+                SqlDataReader dataReader;
+                cnn.Open();
+                dataReader = polecenie.ExecuteReader();
+                if (dataReader.HasRows)
+                {
                     dataReader.Read();
                     user.Id = dataReader.GetInt32(0);
                     user.Firstname = dataReader.GetString(1);
@@ -66,8 +118,54 @@ namespace WebApplication2.ServiceModel
                     user.Bio = dataReader.GetString(7);
 
                     dataReader.Close();
+                }
+                else
+                {
+                    dataReader.Close();
+                    command = String.Format("Select * from Users where Login='{0}'", request.Login);
+                    polecenie = new SqlCommand(command, cnn);
+                    dataReader = polecenie.ExecuteReader();
+                    if (dataReader.HasRows)
+                    {
+                        dataReader.Read();
+                        user.Id = dataReader.GetInt32(0);
+                        user.Firstname = dataReader.GetString(1);
+                        user.Lastname = dataReader.GetString(2);
+                        user.Login = dataReader.GetString(4);
+                        user.Password = dataReader.GetString(5);
+                        user.Avatar = dataReader.GetString(6);
+                        user.Bio = dataReader.GetString(7);
+
+                        dataReader.Close();
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+
+                //update user
+
+                user.Firstname = request.Firstname;
+                user.Lastname = request.Lastname;
+                user.Password = request.Password;
+                user.Avatar = request.Avatar;
+                user.Bio = request.Bio;
+
+                command = String.Format("UPDATE Users set Imie='{0}', Nazwisko='{1}', Password='{2}', Avatar='{3}', Bio='{4}'", request.Firstname, request.Lastname, request.Password, request.Avatar, request.Bio);
+                polecenie = new SqlCommand(command, cnn);
+                dataReader = polecenie.ExecuteReader();
+
+                if (dataReader.RecordsAffected > 0)
+                {
+                    dataReader.Close();
                     cnn.Close();
                     return user;
+                }
+                else
+                {
+                    dataReader.Close();
+                    cnn.Close();
                     return null;
                 }
             }
@@ -87,7 +185,6 @@ namespace WebApplication2.ServiceModel
         public string Firstname { get; set; }
         public string Lastname { get; set; }
         public string Password { get; set; }
-        public string Email { get; set; }
         public string Avatar { get; set; }
         public string Bio { get; set; }
     }
