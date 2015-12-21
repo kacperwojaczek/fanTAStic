3c30c6d0045bb1888fed5e05e85628bf2678f110
kacperwojaczek <wojaczek.kacper@gmail.com>
2015-12-21 23:09:14 +0100

    user posts service refactor

--- API/WebApplication2/WebApplication2.ServiceModel/UserPosts.cs
+++ API/WebApplication2/WebApplication2.ServiceModel/UserPosts.cs
@@ -1,8 +1,10 @@
 ﻿using ServiceStack;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -26,40 +28,42 @@ namespace WebApplication2.ServiceModel
         public List<Int32> Get(UserPostsRequest request)
         {
             List<Int32> posts = new List<Int32>();
-            string connectionString = null;
-            SqlConnection cnn;
-            connectionString = "Server=tcp:fantastic.database.windows.net,1433;Database=fantastic;User ID=qacpiweb@fantastic;Password=nhm554WW;Trusted_Connection=False;Encrypt=True;Connection Timeout=30;";
-            cnn = new SqlConnection(connectionString);
+            var dbConnection = new DatabaseConnector();
+            var paramsList = new List<SqlParameter>();
+            SqlDataReader dataReader;
+            string command;
+
             try
             {
-                string command = String.Format("Select * from Users where Login='{0}'", request.Login);
-                SqlCommand polecenie = new SqlCommand(command, cnn);
-                SqlDataReader dataReader;
-                cnn.Open();
-                dataReader = polecenie.ExecuteReader();
+                SqlParameter loginParam = new SqlParameter("@Login", SqlDbType.VarChar, request.Login.Length);
+                loginParam.Value = request.Login;
+                paramsList.Add(loginParam);
+                command = "Select * from Users where Login=@Login";
+                dataReader = dbConnection.executeCommand(command, paramsList);
+
                 if (dataReader.HasRows)
                 {
                     dataReader.Read();
                     Int32 authorId = dataReader.GetInt32(0);
                     dataReader.Close();
 
-                    command = String.Format("Select id from Wall where Autor={0}", authorId);
-                    polecenie = new SqlCommand(command, cnn);
-                    using (dataReader = polecenie.ExecuteReader())
+                    paramsList.Clear();
+                    SqlParameter authorParam = new SqlParameter("@Author", SqlDbType.Int);
+                    authorParam.Value = authorId;
+                    paramsList.Add(authorParam);
+                    command = "Select id from Wall where Autor=@Author";
+                    dataReader = dbConnection.executeCommand(command, paramsList);
+
+                    while (dataReader.Read())
                     {
-                        while (dataReader.Read())
-                        {
-                            posts.Add(dataReader.GetInt32(0));
-                        }
+                        posts.Add(dataReader.GetInt32(0));
                     }
 
-                    cnn.Close();
                     return posts;
                 }
                 else
                 {
                     dataReader.Close();
-                    cnn.Close();
                     return null;
                 }
             }
@@ -73,17 +77,20 @@ namespace WebApplication2.ServiceModel
         public int Post(UserPostsRequest request)
         {
             UserPost post = new UserPost();
-            string connectionString = null;
-            SqlConnection cnn;
-            connectionString = "Server=tcp:fantastic.database.windows.net,1433;Database=fantastic;User ID=qacpiweb@fantastic;Password=nhm554WW;Trusted_Connection=False;Encrypt=True;Connection Timeout=30;";
-            cnn = new SqlConnection(connectionString);
+            List<Int32> posts = new List<Int32>();
+            var dbConnection = new DatabaseConnector();
+            var paramsList = new List<SqlParameter>();
+            SqlDataReader dataReader;
+            string command;
+
             try
             {
-                string cmd = String.Format("select id from Users where Login='{0}'", request.Login);
-                SqlCommand polecenie = new SqlCommand(cmd, cnn);
-                SqlDataReader dataReader;
-                cnn.Open();
-                dataReader = polecenie.ExecuteReader();
+                SqlParameter loginParam = new SqlParameter("@Login", SqlDbType.VarChar, request.Login.Length);
+                loginParam.Value = request.Login;
+                paramsList.Add(loginParam);
+                command = "select id from Users where Login=@Login";
+                dataReader = dbConnection.executeCommand(command, paramsList);
+
                 dataReader.Read();
 
                 post.authorId = dataReader.GetInt32(0);
@@ -91,15 +98,43 @@ namespace WebApplication2.ServiceModel
                 post.text = request.Content;
                 post.tags = 0;
                 post.reblog = 0;
+                post.attachment = "empty";
 
                 dataReader.Close();
 
-                string command = String.Format("INSERT into Wall (Autor, Tytul, reblog, Tresc, Tagi, attachment, DataPosta) values ('{0}', '{1}', '{2}', '{3}', {4}, '{5}', GETDATE())", post.authorId, post.title, post.reblog, post.text, post.tags, "");
-                polecenie = new SqlCommand(command, cnn);
-                //cnn.Open();
-                dataReader = polecenie.ExecuteReader();
-                cnn.Close();
-                return 200;
+                command = "INSERT into Wall (Autor, Tytul, reblog, Tresc, Tagi, attachment, DataPosta) values (@AuthorId, @Title, @Reblog, @Text, @Tags, @Attach, GETDATE())";
+                // post.authorId, post.title, post.reblog, post.text, post.tags, post.attachment);
+
+                paramsList.Clear();
+                SqlParameter tempParam = new SqlParameter("@AuthorId", SqlDbType.Int);
+                tempParam.Value = post.authorId;
+                paramsList.Add(tempParam);
+                tempParam = new SqlParameter("@Title", SqlDbType.VarChar, post.title.Length);
+                tempParam.Value = post.title;
+                paramsList.Add(tempParam);
+                tempParam = new SqlParameter("@Reblog", SqlDbType.Int, post.reblog);
+                tempParam.Value = post.reblog;
+                paramsList.Add(tempParam);
+                tempParam = new SqlParameter("@Text", SqlDbType.VarChar, post.text.Length);
+                tempParam.Value = post.text;
+                paramsList.Add(tempParam);
+                tempParam = new SqlParameter("@Tags", SqlDbType.Binary);
+                tempParam.Value = BitConverter.GetBytes(post.tags);
+                paramsList.Add(tempParam);
+                tempParam = new SqlParameter("@Attach", SqlDbType.VarChar, post.attachment.Length);
+                tempParam.Value = post.attachment;
+                paramsList.Add(tempParam);
+
+                dataReader = dbConnection.executeCommand(command, paramsList);
+
+                if (dataReader.RecordsAffected > 0)
+                {
+                    return (int)HttpStatusCode.OK;
+                }
+                else
+                {
+                    return (int)HttpStatusCode.InternalServerError;
+                }
             }
             catch (Exception ex)
             {
